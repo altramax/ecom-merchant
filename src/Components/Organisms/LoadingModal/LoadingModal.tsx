5a3d98e6c0feef3fa6cd89db063bed45b69898b5
altramax <ajayiezekiel559@gmail.com>
2024-03-16 01:36:57 +0100

    Loading Adjustments

--- src/Components/Organisms/LoadingModal/LoadingModal.tsx
+++ src/Components/Organisms/LoadingModal/LoadingModal.tsx
@@ -1,24 +1,24 @@
 import LoadingModalStyle from "./LoadingModalstyles";
 import { useAppSelector } from "../../../Redux/Hooks";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const LoadingModal = () => {
   const color = useAppSelector((state) => state.color);
-  const [count, setCount] = useState<number>(1);
+  // const [count, setCount] = useState<number>(1);
 
-  useEffect(() => {
-    const handleCount = setInterval(() => {
-      setCount((prevCount) => {
-        if (prevCount >= 100) {
-          clearInterval(handleCount);
-          return prevCount;
-        } else {
-          return prevCount + 1;
-        }
-      });
-    }, 50);
-    return () => clearInterval(handleCount);
-  }, []);
+  // useEffect(() => {
+  //   const handleCount = setInterval(() => {
+  //     setCount((prevCount) => {
+  //       if (prevCount >= 100) {
+  //         clearInterval(handleCount);
+  //         return prevCount;
+  //       } else {
+  //         return prevCount + 1;
+  //       }
+  //     });
+  //   }, 50);
+  //   return () => clearInterval(handleCount);
+  // }, []);
 
   return (
     <LoadingModalStyle>
